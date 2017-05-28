68bf80b35c2f21437a6edc38c37a80b222342959
Kasia <katarzyna.kulma@gmail.com>
2017-05-28 22:31:33 +0100

    finished gganimate plot

--- uk_police_animation.R
+++ uk_police_animation.R
@@ -11,10 +11,10 @@ library(stringr)
 library(ggmap)
 library(ggplot2)
 
-??ggproto
+
 
 #files <- list.files(recursive = T, pattern = "*.csv")
-files <- dir(recursive = T, pattern = "*.csv", full.names=TRUE)
+#files <- dir(recursive = T, pattern = "*.csv", full.names=TRUE)
 london_files <- dir(recursive = T, pattern = "*metropolitan-street.csv", full.names=TRUE)
 
 london_police_data <- do.call(rbind,lapply(london_files, read.csv))
@@ -29,11 +29,11 @@ map_data <- london_police_data %>%
 
 summary(map_data)
 
-map_data[is.na(map_data), ] <- NULL
+map_data <- map_data[complete.cases(map_data),]
 
 ### creating a map ####
-london_lon = c(min(map_data$Longitude, na.rm = TRUE), max(map_data$Longitude, na.rm = TRUE)) 
-london_lat = c(min(map_data$Latitude, na.rm = TRUE), max(map_data$Latitude, na.rm = TRUE)) 
+#london_lon = c(min(map_data$Longitude, na.rm = TRUE), max(map_data$Longitude, na.rm = TRUE)) 
+#london_lat = c(min(map_data$Latitude, na.rm = TRUE), max(map_data$Latitude, na.rm = TRUE)) 
 
 ?get_map
 
@@ -59,56 +59,63 @@ london_heat_map<- ggmap(london_map, extent = "device") +
 london_heat_map
 
 
-#### animated maps 
+#### animated maps - gganimate alone ####
+devtools::install_github("dgrtwo/gganimate")
 
-devtools::install_github("thomasp85/tweenr")
+library(gganimate)
+library(scales)
+
+
+
+map_anime<- ggmap(london_map, extent = "device") +
+  stat_density_2d(aes(x = Longitude, y = Latitude, frame = Month, 
+                      fill = ..level.., alpha=1),
+                  data=map_data, geom = "polygon") +
+  scale_fill_gradient(low = "blue", high = "red") +
+  scale_alpha(range = c(0.00, 0.5), guide = FALSE) 
+
+gganimate(map_anime)
+
+
+
+#### map animations = tweenr pkg ####
+#devtools::install_github("thomasp85/tweenr")
 library(tweenr)
+library(tidyr)
 
-weapons_1 <- map_data %>% filter(Month == '2016-01')
-weapons_2 <- map_data %>% filter(Month == '2016-02')
-weapons_3 <- map_data %>% filter(Month == '2016-03')
-weapons_4 <- map_data %>% filter(Month == '2016-04')
-weapons_5 <- map_data %>% filter(Month == '2016-05')
-weapons_6 <- map_data %>% filter(Month == '2016-06')
-weapons_7<- map_data %>% filter(Month == '2016-07')
-weapons_8 <- map_data %>% filter(Month == '2016-08')
-weapons_9 <- map_data %>% filter(Month == '2016-09')
-weapons_10 <- map_data %>% filter(Month == '2016-10')
-weapons_11 <- map_data %>% filter(Month == '2016-11')
-weapons_12 <- map_data %>% filter(Month == '2016-12')
-weapons_13 <- map_data %>% filter(Month == '2017-01')
-weapons_14 <- map_data %>% filter(Month == '2017-02')
-weapons_15 <- map_data %>% filter(Month == '2017-03')
+str(map_data)
+summary(map_data)
+table(map_data$Month)
 
-?tween_states
+is.na(map_data) %>% table()
+map_data <- droplevels(map_data)
 
-data <- tween_states(list(paste0("weapons_",1:15)), 3, 1, 'cubic-in-out', 100)
+london_maps_n <- map_data %>%
+  group_by(Month) %>%
+  nest() 
+
+str(london_maps_n$data)
+summary(london_maps_n$data)
+
+### error: cannot send dataframes of varying lengths to tween_states()
+tween_london_maps <- tween_states(london_maps_n$data, tweenlength = 1,
+                                  statelength = 0.5, ease = "sine-out", nframe = 200) %>%
+  as.tibble()
 
 
-##### example 
-library(tweenr)
 
-gapminder_edit <- gapminder %>%
-  arrange(country, year) %>%
-  select(gdpPercap,lifeExp,year,country, continent, pop) %>%
-  rename(x=gdpPercap,y=lifeExp,time=year,id=country) %>%
-  mutate(ease="linear")
 
-gapminder_tween <- tween_elements(gapminder_edit,
-                                  "time", "id", "ease", nframes = 300) %>%
-  mutate(year = round(time), country = .group) %>%
-  left_join(gapminder, by=c("country","year","continent")) %>%
-  rename(population = pop.x)
 
-p2 <- ggplot(gapminder_tween,
-             aes(x=x, y=y, frame = .frame)) +
-  geom_point(aes(size=population, color=continent),alpha=0.8) +
-  xlab("GDP per capita") +
-  ylab("Life expectancy at birth") +
-  scale_x_log10(labels=comma)
 
-gganimate(p2, filename="gapminder-tween.gif", title_frame = FALSE, interval = 0.05)
-For another nice example using the tweenr package, check out this blog post by Jake 
+
+
+
+
+
+
+
+
+
 
 
 
